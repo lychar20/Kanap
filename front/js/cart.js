d2f13eae20210d6ee59f32232318a0c125868453
Charly Miampunina Mampuya <charly.mampuya@yahoo.fr>
2022-11-27 18:52:32 +0100

    calcul

--- front/js/cart.js
+++ front/js/cart.js
@@ -22,12 +22,12 @@ console.log("sofa", sofa);
 
 let panierentier = []
 
+
 if (sofa != null ) {
-    for (canap of sofa) { // for canap of sofa
 
-        fetch(`http://localhost:3000/api/products/${canap.id}`) 
+  sofa.forEach(canap => {
 
-        
+    fetch(`http://localhost:3000/api/products/${canap.id}`)         
 
         .then(function(res) {
             if (res.ok) {
@@ -35,7 +35,7 @@ if (sofa != null ) {
             }
         })
         .then(function(value) {
-            console.log("value", value);    
+            //console.log("value", value);    
             
              panierentier.push({
              id: value._id,    // talbeau value
@@ -44,59 +44,52 @@ if (sofa != null ) {
              prix: value.price,    // tableau value
              quantite: canap.nombre,   //canap.nombre
              couleur: canap.couleur    //canap.couleur
-            })
-           
+            })  
+
+            console.log(canap)
+            console.log(panierentier)
             
-            console.log("panierentier", panierentier);
-
             
-             for (let x of panierentier)   {
 
-                
+            document.querySelector('#cart__items').innerHTML += `<article class="cart__item" data-id="${value._id}" data-color="${canap.couleur}">
+              <div class="cart__item__img">
+              <img src="${value.imageUrl}">
+              </div>
+              <div class="cart__item__content">
+                <div class="cart__item__content__description">
+                  <h2>${value.name} </h2>
+                  <p>${canap.couleur}</p>
+                  <p>${value.price} euros </p>
+                </div>
+                <div class="cart__item__content__settings">
+                  <div class="cart__item__content__settings__quantity">
+                    <p>${canap.nombre} </p>
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${canap.nombre}">
+                  </div>
+                  <div class="cart__item__content__settings__delete">
+                    <p class="deleteItem">Supprimer</p>
+                  </div>
+                </div>
+              </div>
+            </article>`;   
 
-                    document.querySelector('#cart__items').innerHTML += `<article class="cart__item" data-id="${x.id}" data-color="${x.couleur}">
-                    <div class="cart__item__img">
-                      <img src="${x.image} " alt="${x.altTxt}">
-                    </div>
-                    <div class="cart__item__content">
-                      <div class="cart__item__content__description">
-                        <h2>${x.nom} </h2>
-                        <p>${x.couleur}</p>
-                        <p>${x.prix} euros </p>
-                      </div>
-                      <div class="cart__item__content__settings">
-                        <div class="cart__item__content__settings__quantity">
-                          <p>${x.quantite} </p>
-                          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                        </div>
-                        <div class="cart__item__content__settings__delete">
-                          <p class="deleteItem">Supprimer</p>
-                        </div>
-                      </div>
-                    </div>
-                  </article>`; 
 
-             }
-             
-
+              
              })
+          .catch(function(err) {
+            
+          })   
+    
+  });
+    
 
-             
-   
-   .catch(function(err) {
-     
-   }) 
-
-        
+    console.log("panierentier", panierentier);    
+    
+    //
 
 
-    }    // for canap of sofa
 
-
-    //for pour les panier
-
- 
-
+    //
 
 } else {
     document
@@ -107,3 +100,25 @@ if (sofa != null ) {
 
 
 
+// Fonctions de calcul
+
+
+
+  let prixTotalCalcul = []
+
+
+for (let m = 0; m < panierentier.length; m++ ){
+  let prixProduitsDanslePanier = panierentier[m].prix;
+  console.log ("Test1",prixProduitsDanslePanier );
+  console.log("Test2", panierentier.length);
+  console.log ("Test3", panierentier );
+
+  prixTotalCalcul.push(prixProduitsDanslePanier)
+
+  console.log("prixTotal", prixTotalCalcul);
+} 
+
+
+
+ 
+ 
